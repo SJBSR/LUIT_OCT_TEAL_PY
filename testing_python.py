aead4ae199ae96d08a26b271a225cacf83fa60df
SJBSR <sjb.sr@outlook.com>
2025-10-30 19:54:19 -0400

    Random Password Generator

--- testing_python.py
+++ testing_python.py
@@ -1,1 +1,24 @@
-print ("Hello, World!")
+# Random Password Generator using Python
+
+#import the necessary libraries
+import random
+import string
+
+#input the length of the password
+length = int(input("Enter the length of the password: "))
+
+#Define data to be used in the password
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+numbers = string.digits
+symbols = string.punctuation   
+
+#combine the data
+all = lower + upper + numbers + symbols
+
+#use random to generate the password
+password = "".join(random.sample(all, length))
+
+#print the password
+print("Your password is: ", password)
+
